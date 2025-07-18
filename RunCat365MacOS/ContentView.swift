dd73ac550b973d3aa6fafa2097807860d3a6c176
Yash Prajapati <workwidyash@gmail.com>
2025-07-18 11:48:53 +0530

    Initial Commit

---
+++ RunCat365MacOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunCat365MacOS
+//
+//  Created by Yash Prajapati on 18/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunCat365MacOS/RunCat365MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunCat365MacOSApp.swift
+//  RunCat365MacOS
+//
+//  Created by Yash Prajapati on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct RunCat365MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
